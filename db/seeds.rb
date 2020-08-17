b7658ff162947b0148eccb69eb3610b4e594dc1f
Kamil Dziekan <dziekan30@gmail.com>
2020-08-17 12:54:51 -0500

    updated seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,56 +6,56 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# dog = Dog.new(name: "Atos", breed_description: "sznaucer", bio: "Pretty agresive dog", active_status: true, size: 2)
-# dog.save
+dog = Dog.new(name: "Atos", breed_description: "sznaucer", bio: "Pretty agresive dog", active_status: true, size: 2)
+dog.save
 
-# dog = Dog.new(name: "Lexi", breed_description: "Golden Retriever", bio: "Very friendly dog. Love to play !!!", active_status: true, size: 3, user_id: 1, price: 100, address: "215 Ohio St", city: "Chicago IL", zipcode: "60654")
-# dog.save
+dog = Dog.new(name: "Lexi", breed_description: "Golden Retriever", bio: "Very friendly dog. Love to play !!!", active_status: true, size: 3, user_id: 1, price: 100, address: "215 Ohio St", city: "Chicago IL", zipcode: "60654")
+dog.save
 
-# breed = Breed.new(name: "Golden Retriever")
-# breed.save
+breed = Breed.new(name: "Golden Retriever")
+breed.save
 
-# breed = Breed.new(name: "Siberian Husky")
-# breed.save
+breed = Breed.new(name: "Siberian Husky")
+breed.save
 
-# breed = Breed.new(name: "Shiba Inu")
-# breed.save
+breed = Breed.new(name: "Shiba Inu")
+breed.save
 
-# breed = Breed.new(name: "Rottweiler")
-# breed.save
+breed = Breed.new(name: "Rottweiler")
+breed.save
 
-# breed = Breed.new(name: "Dobermann")
-# breed.save
+breed = Breed.new(name: "Dobermann")
+breed.save
 
-# breed = Breed.new(name: "Shih Tzu")
-# breed.save
+breed = Breed.new(name: "Shih Tzu")
+breed.save
 
-# breed = Breed.new(name: "Samoyed")
-# breed.save
+breed = Breed.new(name: "Samoyed")
+breed.save
 
-# breed = Breed.new(name: "Yorkshire Terrier")
-# breed.save
+breed = Breed.new(name: "Yorkshire Terrier")
+breed.save
 
-# breed = Breed.new(name: "Pembroke Welsh Corgi")
-# breed.save
+breed = Breed.new(name: "Pembroke Welsh Corgi")
+breed.save
 
-# breed = Breed.new(name: "Jack Russell Terrier")
-# breed.save
+breed = Breed.new(name: "Jack Russell Terrier")
+breed.save
 
-# breed = Breed.new(name: "St. Bernard")
-# breed.save
+breed = Breed.new(name: "St. Bernard")
+breed.save
 
-# breed = Breed.new(name: "Basenji")
-# breed.save
+breed = Breed.new(name: "Basenji")
+breed.save
 
-# breed = Breed.new(name: "Australian Shepherd")
-# breed.save
+breed = Breed.new(name: "Australian Shepherd")
+breed.save
 
-# breed = Breed.new(name: "Chinese Crested Dog")
-# breed.save
+breed = Breed.new(name: "Chinese Crested Dog")
+breed.save
 
-# breed = Breed.new(name: "Boston Terrier")
-# breed.save
+breed = Breed.new(name: "Boston Terrier")
+breed.save
 
-# breed = Breed.new(name: "Border Collie")
-# breed.save
+breed = Breed.new(name: "Border Collie")
+breed.save

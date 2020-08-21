30ea104cc4940db2cc5bdab97e94e05b12bd017d
Kamil Dziekan <dziekan30@gmail.com>
2020-08-20 20:49:07 -0500

    lol

--- app/twilio/sms.rb
+++ app/twilio/sms.rb
@@ -1,10 +1,10 @@
 
-# @client = Twilio::REST::Client.new(Rails.application.credentials.dig(:twilio, :account_sid), Rails.application.credentials.dig(:twilio, :auth_token))
+@client = Twilio::REST::Client.new(Rails.application.credentials.dig(:twilio, :account_sid), Rails.application.credentials.dig(:twilio, :auth_token))
 
-# message = @client.messages.create(
-#                              body: 'Hi there!',
-#                              from: '+12563804814',
-#                              to: '+13124681623'
-#                            )
+message = @client.messages.create(
+                             body: 'Hi there!',
+                             from: '+12563804814',
+                             to: '+13124681623'
+                           )
 
-# puts message.sid
+puts message.sid

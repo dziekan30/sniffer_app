01e1c3cd79261df930a7e962c30d42d6afcaac84
Kamil Dziekan <dziekan30@gmail.com>
2020-08-30 23:07:55 -0500

    msg

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://sniffer.netlify.app/', 'http://localhost:8080' 
+    origins  'http://localhost:8080' 
+    # origins 'https://sniffer.netlify.app/', 'http://localhost:8080' 
 
     resource '*',
       headers: :any,
